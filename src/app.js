3637a94d1b971b07f6374c80c5c7a785123e1f03
Ashok Babu Marthu <ashokmarthu999@gmail.com>
2025-12-06 22:18:15 +0530

    Middlewares and Error Handling

--- src/app.js
+++ src/app.js
@@ -1,15 +1,31 @@
 const express = require("express");
 const app = express();
+const { adminAuth, userAuth } = require("./middleware/auth");
 const port = 4000;
 
-app.get('/', (req, res) => {
-  res.send('Hello From Server!')
-})
+app.use("/admin", adminAuth);
+app.use("/getUserData", (req, res) => {
+  try {
+    throw new Error("hjdkldl");
+  } catch (err) {
+    res.status(500).send("some Error contact support team");
+  }
+});
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("some Error occured");
+  }
+});
 
-app.get('/test', (req, res) => {
-  res.send('Hello From Test Server!')
-})
-
+app.get("/user", userAuth, (req, res) => {
+  res.send("User Data sent");
+});
+app.get("/admin/getAllData", (req, res) => {
+  res.send("All Data sent");
+});
+app.get("/admin/deleteUser", (req, res) => {
+  res.send("Deleted a user");
+});
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
