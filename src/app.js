6c510a6fba92a716c9357f960f6d31820a5929f6
Ashok Babu Marthu <ashokmarthu999@gmail.com>
2025-12-11 22:56:10 +0530

    delete & update user from the database

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.post("/signup", async (req, res) => {
 app.get("/user", async (req, res) => {
   const userEmail = req.body.emailId;
   try {
-    const user = await User.findOne({ emailId: userEmail })
+    const user = await User.findOne({ emailId: userEmail });
     if (!user) {
       res.status(404).send("User not found");
     } else res.send(user);
@@ -29,6 +29,31 @@ app.get("/feed", async (req, res) => {
   try {
     const users = await User.find({});
     res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const deletedUser = await User.findByIdAndDelete(userId);
+    if (!deletedUser) {
+      res.status(404).send("User not found");
+    } else res.send("deleted user successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const isUserExist = await User.findByIdAndUpdate({_id:userId}, data);
+    if (!isUserExist) {
+      res.status(404).send("User not found");
+    } else res.send("updated user successfully");
   } catch (err) {
     res.status(400).send("Something went wrong");
   }
